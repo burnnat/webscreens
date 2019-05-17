5708b91dc4d84b49ebc9fac897adb74286327bd0
Nat Burns <nbaccount@burnskids.com>
2019-05-17 19:30:44 +0000

    Preload images before rotating.

--- src/public/index.js
+++ src/public/index.js
@@ -1,10 +1,40 @@
+let current = null;
+let next = null;
+
+function preloadImage(imageId) {
+    const el = document.createElement('div');
+    el.style = `background-image: url(/api/image/${imageId})`;
+    el.className = 'slide next';
+
+    document.body.appendChild(el);
+
+    return el;
+}
+
+function activate(el) {
+    if (el != null) {
+        el.className = 'slide current';
+    }
+}
+
+function deactivate(el) {
+    if (el != null) {
+        document.body.removeChild(el);
+    }
+}
+
 function advance() {
     fetch('/api/next')
         .then((resp) => resp.json())
         .then((result) => {
-            document.getElementById('current').style = `background-image: url(/api/image/${result.value})`;
+            activate(next);
+            deactivate(current);
+            current = next;
+            next = preloadImage(result.value);
         });
 }
 
-setInterval(advance, 2000);
+setInterval(advance, 15000);
+
+advance();
 advance();

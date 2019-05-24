8bc864bd86cc9d1fd1f309f79d14bfc0fb22e0d7
Nat Burns <nbaccount@burnskids.com>
2019-05-24 11:46:30 +0000

    Allow specifying slide duration in URL.

--- public/index.js
+++ public/index.js
@@ -1,6 +1,13 @@
 var current = null;
 var next = null;
 
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
 function preloadImage(imageId) {
     var el = document.createElement('div');
     el.style.backgroundImage = 'url(/api/image/' + imageId + ')';
@@ -52,10 +59,24 @@ function stop() {
     clearInterval(intervalId);
 }
 
-window.onload = function() {
-    intervalId = setInterval(advance, 5000);
+function getDuration() {
+    duration = getUrlParameter('duration');
 
+    if (duration === null) {
+        duration = 30;
+    }
+    else {
+        duration = parseInt(duration, 10);
+    }
+
+    // Convert from seconds to milliseconds.
+    return duration * 1000;
+}
+
+window.onload = function() {
+    intervalId = setInterval(advance, getDuration());
     next = document.getElementById('init');
+    
     advance();
     advance();
 };

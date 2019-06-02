0b5cd9c89d4ce2ada11250ef66a23f6a4fa045e5
Nat Burns <nbaccount@burnskids.com>
2019-06-02 17:30:41 +0000

    Use configuration file, add username and password.

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,8 @@ import { union } from 'lodash';
 import * as yargs from 'yargs'
 
 let argv =  yargs
-	.option('p', {
+	.config()
+	.option('b', {
 		alias: 'port',
 		default: 3000,
 		describe: 'Port to bind on',
@@ -16,12 +17,24 @@ let argv =  yargs
 		describe: 'Directory of slideshow images',
 		type: 'string'
 	})
+	.option('u', {
+		alias: 'username',
+		describe: 'OurGroceries username',
+		type: 'string'
+	})
+	.option('p', {
+		alias: 'password',
+		describe: 'OurGroceries password',
+		type: 'string'
+	})
 	.help()
 	.argv;
 
 export default class Config {
-	public static port: number = argv.p;
+	public static port: number = argv.b;
 	public static slides: string = argv.d;
+	public static username: string = argv.u;
+	public static password: string = argv.p;
 	public static routes: string = path.join(__dirname, '../routes/**/*.js');
 	public static globFiles(location: string): string[] {
 		return union([], sync(location));
